9da2466ce3bb9b0a73e1b7b301999e9a2d7a4d5c
Rodolfo Bermúdez-Neubauer <intellego-rbn@users.noreply.github.com>
2019-03-27 14:49:58 -0300

    SALE Blanket Order Type

---
+++ l10n_cl_sale_order_type/models/sale_blanket_order.py
@@ -0,0 +1,28 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+class SaleBlanketOrder(models.Model):
+    _name = 'sale.order.blanket'
+    _description = 'Blanket Sale Order'
+
+    #@api.model
+    #def _get_domain_sequence_id(self):
+    #    seq_type = self.env.ref('sale.seq_sale_order')
+    #    return [('code', '=', seq_type.code)]
+
+
+    name = fields.Char(string='Name', required=True, translate=True)
+    description = fields.Text(string='Description', translate=True)
+    active = fields.Boolean(
+        'Active', default=True)
+
+    #sequence_id = fields.Many2one(
+    #    comodel_name='ir.sequence', string='Entry Sequence', copy=False,
+    #    domain=_get_domain_sequence_id)
+
+    company_id = fields.Many2one('res.company', 'Company')
+    sale_order_type_id = fields.Many2one('sale.order.type', 'Sale Order Type')
+    payment_term_id = fields.Many2one('account.payment.term', 'Payment Term')
+    pricelist_id = fields.Many2one('product.pricelist', 'Pricelist')
+    incoterm_id = fields.Many2one('account.incoterms', 'Incoterm')
