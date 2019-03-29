272100ab10d36022175847f7c53a430f75db3432
Rodolfo Bermúdez-Neubauer <intellego-rbn@users.noreply.github.com>
2019-03-29 04:56:42 -0300

    Update sale_order.py

--- l10n_cl_sale_order_type/models/sale_order.py
+++ l10n_cl_sale_order_type/models/sale_order.py
@@ -7,13 +7,9 @@ class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     def _get_partner_ids(self):
-        customers = False    
-        if self.blanket_partner_category_ids:
+        customers = self.env['res.partner'].search([('customer', '=', True)])   
+        if self.blanket_partner_category_ids.id > 0:
             customers = self.env['res.partner'].search([('customer', '=', True), ('category_id', '=', self.blanket_partner_category_ids.id)])
-        if not customers: 
-            customers = self.env['res.partner'].search([('customer', '=', True)])
-            if not customers:
-                customers = []
         return customers
 
     def _get_order_type(self):
